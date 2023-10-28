9d4f3fabf98b2248696b32b51f7381183cb862a5
Furkan Bilal <bilalyigit594@gmail.com>
2023-10-28 15:27:52 +0300

    Auth line erased

--- My3rdBlog/Program.cs
+++ My3rdBlog/Program.cs
@@ -15,41 +15,6 @@ namespace Blog
         {
             var host = CreateWebHostBuilder(args).Build();
 
-            try
-            {
-                var scope = host.Services.CreateScope();
-
-                var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-                var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-                ctx.Database.EnsureCreated();
-
-                var adminRole = new IdentityRole("Admin"); //Create "Admin" role to get claimed
-                if (!ctx.Roles.Any())
-                {
-                    //create a role
-                    roleMgr.CreateAsync(adminRole).GetAwaiter().GetResult();
-                }
-
-                if (!ctx.Users.Any(u => u.UserName == "admin")) //If there's no user role named "admin" create
-                {
-                    //create an admin
-                    var adminUser = new IdentityUser
-                    {
-                        UserName = "admin",
-                        Email = "admin@test.com"
-                    };
-                    var result = userMgr.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
-                    //add role to user
-                    userMgr.AddToRoleAsync(adminUser, adminRole.Name).GetAwaiter().GetResult();//Assign "admin" user to "Admin" role
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
             host.Run();
         }
 
